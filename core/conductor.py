7ae70399de581021c62a6af2e13fdad62ee44116
IKEA0x90 <ivanchousKutovoy@gmail.com>
2025-07-18 19:49:08 +0400

    this works, but album images are processed separately

--- core/conductor.py
+++ core/conductor.py
@@ -62,12 +62,18 @@ class Conductor:
             else:
                 role = 'assistant' if user == self.username else 'user'
 
-            message_text = message.text or ''
+            message_text = message.text or message.caption or''
             
             entities = message.parse_entities(types=[
                 'mention',
                 'url'
             ])
+
+            if not entities:
+                entities = message.parse_caption_entities(types=[
+                    'mention',
+                    'url'
+                ])
 
             ping = False
 
